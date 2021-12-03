118d7df178df3de920ffa147ec5b470ae75310dd
BᵣₐyDₑₙ <73177244+BrayDenXD@users.noreply.github.com>
2021-12-03 08:09:34 +0530

    Create id.py

---
+++ player/id.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.command('id'))
+def ids(_,message):
+  reply = message.reply_to_message
+  if reply:
+    message.reply_text(f"**Your ID**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ID**: `{reply.from_user.id}`\n**Chat ID**: `{message.chat.id}`")
+  else:
+    message.reply(f"**Your id**: `{message.from_user.id}`\n**chat id**: `{message.chat.id}`")
