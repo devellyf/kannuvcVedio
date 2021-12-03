3d1852734d5aca0aad281b02c980ac1ac01e3ded
BᵣₐyDₑₙ <73177244+BrayDenXD@users.noreply.github.com>
2021-12-03 15:30:52 +0530

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -3,8 +3,8 @@ from config import ASSISTANT_NAME
 
 HELP_TEXT =f"**SETTINGS** \n\n`Click on the buttons below to get documentation about specific modules..`"
 START_TEXT = '👋 `Hey My Name Is` **@{ASSISTANT_NAME}** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
-STREAM_TEXT = '**COMMANDS** \n\n`/stream` **Reply to Video or File That You Want To stream In Vc.** \n\n`/stream` **try /stream [yt link]..**'
-ENDST_TEXT =f"**COMMANDS** \n\n`/stopstream` **Stop the stream.**"
-PASTE_TEXT = 'HELLO HUYS'
-VSONG_TEXT = '"HELLP ME GUYS'
-TGPK_TEXT =f"his g"
+STREAM_TEXT = '**COMMANDS** \n\n`/stream` - Reply to Video or File That You Want To stream In Vc. \n\n`/stream` - use [yt link] \n\n`/audio` - reply to any audio file or utube url.. \n\n`/endstream` - Stop the stream'
+INFO_TEXT =f"**COMMANDS** \n\nGet information about something!\n\n`/info` - reply to any user!\n\n`/whois` get the information of an user!"
+PASTE_TEXT = '**COMMANDS** \n\nPaste some texts or documents on pasting website!\n\n`/paste` Reply To Any File!'
+VSONG_TEXT = '**COMMANDS** \n\nDo You Want Video Song!\n\n`/video` **[Song Name]** Search Your Favourite Song!\n\n`/vsong` **[Song Name]** Try!'
+TGPK_TEXT =f"**COMMANDS** \n\nDo as you wish with telegra.ph module!\n\n`/telegraph` Reply to a supported media file!\n\n`/tgm` Reply to a any msg or file."
