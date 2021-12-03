90d2435b562359baa1fbe6ead28b25dd68d01da2
BᵣₐyDₑₙ <73177244+BrayDenXD@users.noreply.github.com>
2021-12-03 15:03:29 +0530

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -3,7 +3,7 @@ from config import ASSISTANT_NAME
 
 HELP_TEXT =f"**SETTINGS** \n\n`Click on the buttons below to get documentation about specific modules..`"
 START_TEXT = '👋 `Hey My Name Is` **@{ASSISTANT_NAME}** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
-ABOUT_TEXT = '**COMMANDS** \n\n`/stream` **Reply to Video or File That You Want To stream In Vc.** \n\n`/stream` **try /stream [yt link]..**'
+STREAM_TEXT = '**COMMANDS** \n\n`/stream` **Reply to Video or File That You Want To stream In Vc.** \n\n`/stream` **try /stream [yt link]..**'
 ENDST_TEXT =f"**COMMANDS** \n\n`/stopstream` **Stop the stream.**"
 PASTE_TEXT = 'HELLO HUYS'
 VSONG_TEXT = '"HELLP ME GUYS'
